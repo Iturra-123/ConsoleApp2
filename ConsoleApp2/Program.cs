e277a28dae1a0ea64e0fd9a81b8755cc646e914a
leit07001 <leit07001@E-PW05C37S>
2024-09-12 12:17:55 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace U2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Du efe ufueufeu");
+            Console.WriteLine("Du har lärt dig tecken som \" och \\");
+        }
+    }
+}
